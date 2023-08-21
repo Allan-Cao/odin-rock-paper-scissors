3aa16a0f00ecfe0de765c10e7b6ef82ae7729a2f
Allan Cao <allan@allancao.ca>
2023-08-20 22:56:17 -0600

    Implement playRound logic

--- game.js
+++ game.js
@@ -4,4 +4,30 @@ function getComputerChoice(){
     return game_choices[Math.floor(Math.random() * 3)]
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection == computerSelection) {
+        return 'Tie!'
+    }
+    switch (playerSelection) {
+        case 'rock':
+            if (computerSelection == 'paper') {
+                return 'You Lose! Rock loses to Paper'
+            }
+            return 'You Win! Rock beats Scissors'
+        case 'paper':
+            if (computerSelection == 'scissors') {
+                return 'You Lose! Paper loses to Scissors'
+            }
+            return 'You Win! Paper beats Rock'
+        case 'scissors':
+            if (computerSelection == 'rock') {
+                return 'You Lose! Scissors loses to Rock'
+            }
+            return 'You Win! Scissors beats Paper'
+        default:
+            return 'Unknown player selection'
+    }
+}
